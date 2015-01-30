c850f5578b070ae901540fc85dfee1ebe28a5f5a
mikelangston <mikemac8203@gmail.com>
2015-01-29 20:41:03 -0500

    added scaffold tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :tasks
+
   devise_for :users
   get '/about' => 'pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
